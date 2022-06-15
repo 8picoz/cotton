661ad806dc6bf51999a9213a6b5cb175b51713c7
tata <8picoz@tata.pw>
2022-06-16 03:20:20 +0900

    [chore] clean debug message

--- src/renderer/validation_layer.rs
+++ src/renderer/validation_layer.rs
@@ -2,7 +2,7 @@ use std::ffi::{c_void, CStr, CString};
 use ash::{Entry, Instance, vk};
 use ash::extensions::ext::DebugUtils;
 use ash::prelude::VkResult;
-use ash::vk::{DebugUtilsMessengerCreateInfoEXT, DebugUtilsMessengerEXT};
+use ash::vk::{DebugUtilsMessageSeverityFlagsEXT, DebugUtilsMessageTypeFlagsEXT, DebugUtilsMessengerCreateInfoEXT, DebugUtilsMessengerEXT};
 use log::debug;
 use tobj::LoadError::NormalParseError;
 
@@ -14,6 +14,19 @@ pub struct ValidationLayer {
 }
 
 impl ValidationLayer {
+    pub fn require_debug_utils_extension_names_ptr() -> Vec<*const i8> {
+        vec![DebugUtils::name().as_ptr()]
+    }
+
+    pub fn require_validation_layer_extension_names() -> Vec<&str> {
+        vec!["VK_LAYER_KHRONOS_validation"]
+    }
+
+    pub fn require_validation_layer_extension_names_ptr() -> Vec<*const i8> {
+        //ライフタイムがダメでは？
+        Self::require_validation_layer_extension_names().into_iter().map(|name| CString::new(name).unwrap().as_ptr()).collect()
+    }
+
     pub fn new_default(entry: &Entry, instance: &Instance) -> VkResult<Self> {
         debug!("Enable validation layer");
 
@@ -29,7 +42,25 @@ impl ValidationLayer {
         })
     }
 
-    fn populate_debug_messenger_create_info() -> DebugUtilsMessengerCreateInfoEXT {
+    pub fn check_validation_layer_support(entry: &Entry) {
+        for required in Self::require_validation_layer_extension_names() {
+            let found = entry
+                .enumerate_instance_layer_properties()
+                .unwrap()
+                .iter()
+                .any(|layer| {
+                    let name = unsafe { CStr::from_ptr(layer.layer_name.as_ptr()) };
+                    let name = name.to_str().expect("Failed to get layer name pointer");
+                    required == name
+                });
+
+            if !found {
+                panic!("Validation layer not supported: {}", required);
+            }
+        }
+    }
+
+    pub fn populate_debug_messenger_create_info() -> DebugUtilsMessengerCreateInfoEXT {
         DebugUtilsMessengerCreateInfoEXT::builder()
             .message_severity(
                 vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE
@@ -57,15 +88,30 @@ impl Drop for ValidationLayer {
 }
 
 unsafe extern "system" fn vulkan_debug_callback(
-    _message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
-    _message_type: vk::DebugUtilsMessageTypeFlagsEXT,
+    message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+    message_type: vk::DebugUtilsMessageTypeFlagsEXT,
     p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT,
     _p_user_data: *mut c_void,
 ) -> vk::Bool32 {
-    let data = *p_callback_data;
-    let message = CStr::from_ptr(data.p_message).to_string_lossy();
+    let severity = match _message_severity {
+        DebugUtilsMessageSeverityFlagsEXT::VERBOSE => "VERBOSE",
+        DebugUtilsMessageSeverityFlagsEXT::WARNING => "WARNING",
+        DebugUtilsMessageSeverityFlagsEXT::ERROR => "ERROR",
+        DebugUtilsMessageSeverityFlagsEXT::INFO => "INFO",
+        _ => "???",
+    };
 
-    debug!("validation layer: {:?}", message);
+    let types = match message_type {
+        DebugUtilsMessageTypeFlagsEXT::GENERAL => "GENERAL",
+        DebugUtilsMessageTypeFlagsEXT::PERFORMANCE => "PERFORMANCE",
+        DebugUtilsMessageTypeFlagsEXT::VALIDATION => "VALIDATION",
+        _ => "???",
+    };
+
+    let data = *p_callback_data;
+    let message = CStr::from_ptr(data.p_message);
+
+    debug!("validation layer: [{}, {}] {:?}", severity, types, message);
 
     vk::FALSE
 }
