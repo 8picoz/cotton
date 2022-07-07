bf19b364201ec9e7bdf89cb49e2429bddb9680cd
tata <8picoz@tata.pw>
2022-07-07 20:16:40 +0900

    [feat] Create Device memory allocator

---
+++ src/buffer.rs
@@ -0,0 +1,75 @@
+use ash::Device;
+use ash::vk::{Buffer, BufferCreateInfo, BufferUsageFlags, DeviceMemory, DeviceSize, MemoryAllocateFlags, MemoryAllocateFlagsInfo, MemoryAllocateInfo, MemoryPropertyFlags, PhysicalDeviceMemoryProperties, SharingMode};
+
+pub struct Buffers {
+    pub raw: Buffer,
+    pub size: DeviceSize,
+    pub memory: DeviceMemory,
+}
+
+impl Buffers {
+    pub fn new(
+        size: DeviceSize,
+        usage: BufferUsageFlags,
+        memory_properties: MemoryPropertyFlags,
+        device: &Device,
+        device_memory_properties: PhysicalDeviceMemoryProperties,
+    ) -> Self {
+        let buffer_info = BufferCreateInfo::builder()
+            .size(size)
+            .usage(usage)
+            .sharing_mode(SharingMode::EXCLUSIVE)
+            .build();
+
+        let raw = unsafe {
+            device.create_buffer(&buffer_info, None).unwrap()
+        };
+
+        //メモリサイズやアライメントなどの確保に必要な情報を持つ構造体
+        let memory_requirements = unsafe {
+            device.get_buffer_memory_requirements(raw)
+        };
+
+        let mut memory_type_index = 0;
+
+        for i in 0..device_memory_properties.memory_type_count {
+            if (type_bits & 1) == 1 {
+                //PhysicalDeviceMemoryPropertiesの中からmemory_propertiesと同一のものを探す
+                if (device_memory_properties.memory_types[i as usize].property_flags & memory_properties) == memory_properties {
+                    memory_type_index = i;
+                }
+            }
+        }
+
+        let mut memory_allocate_flags_info = MemoryAllocateFlagsInfo::builder()
+            //SHADER_DEVICE_ADDRESSの指定とvkGetDeviceMemoryOpaqueCaptureAddressでアドレスを取得できるようになる
+            .flags(MemoryAllocateFlags::DEVICE_ADDRESS)
+            .build();
+
+        let mut allocate_info = MemoryAllocateInfo::builder();
+
+        //SHADER_DEVICE_ADDRESSはvkGetBufferDeviceAddressからバッファのデバイスアドレスを取得することができ、それを使用することでシェーダー内からアクセスすることが出来る
+        if usage.contains(BufferUsageFlags::SHADER_DEVICE_ADDRESS) {
+            allocate_info = allocate_info.push_next(&mut memory_allocate_flags_info);
+        }
+
+        let allocate_info = allocate_info
+            .allocation_size(memory_requirements.size)
+            .memory_type_index(memory_type_index)
+            .build();
+
+        let memory = unsafe {
+            device.allocate_memory(&allocate_info, None).unwrap()
+        };
+
+        unsafe {
+            device.bind_buffer_memory(raw, memory, 0).unwrap()
+        }
+
+        Self {
+            raw,
+            size,
+            memory,
+        }
+    }
+}
--- src/renderer/acceleration_structures.rs
+++ src/renderer/acceleration_structures.rs
@@ -1,9 +1,33 @@
+use ash::extensions::khr::AccelerationStructure;
+use ash::vk::AabbPositionsKHR;
+use crate::renderer::backends::Backends;
+
 pub struct AccelerationStructures {
+    pub acceleration_structure: AccelerationStructure,
 
 }
 
 impl AccelerationStructures {
-    pub fn new() -> Self {
-        
+    pub fn new(backends: &Backends) -> Self {
+        let acceleration_structure
+            = AccelerationStructure::new(&backends.instance, &backends.device);
+
+        //bottom-level
+        let aabb = AabbPositionsKHR::builder()
+            .min_x(-1.0)
+            .max_x(1.0)
+            .min_y(-1.0)
+            .min_z(-1.0)
+            .max_z(1.0)
+            .build();
+
+
+        Self {
+            acceleration_structure,
+        }
+    }
+
+    pub fn create_bottom_acceleration() ->  {
+
     }
 }
